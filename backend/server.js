ac524ec45d9eaa019c3f4c71c0cd6745348edac5
VaibhavAttre <vaibhav.attre@gmail.com>
2025-02-26 20:08:06 -0800

    fixing server bug

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
@@ -32,7 +32,7 @@ app.get("/api/random-image", async (req, res) => {
     }
 });
 
-//Server listening on PORT (set to 5000 right now)
-app.listen(PORT, () => {
-    console.log(`Server is successfully running on port ${PORT}`);
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server is successfully running on port ${PORT}`);
 });
